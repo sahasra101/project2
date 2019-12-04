4304de42b2882bafb52e650bdb8517957af4f626
sahasra101 <ajaykiri7@gmail.com>
2019-12-04 14:27:23 -0500

    added zillow api to apiroutes and google maps to userinput files

--- public/js/userInput.js
+++ public/js/userInput.js
@@ -28,6 +28,16 @@ var yearOneYield = 0;
 var yieldToFixed = 0;
 
 $(document).ready(function() {
+  $.ajax({
+    url: "/api/zillow",
+    method: "GET",
+    success: function(data) {
+      console.log(data);
+    },
+    error: function(err) {
+      console.log(err);
+    }
+  });
   //   Initial variables
 
   $("#submit").on("click", function(event) {
@@ -112,3 +122,29 @@ $(document).ready(function() {
     $(".results").append("Yield: " + yieldToFixed + "%");
   });
 });
+
+var map;
+var marker;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 14,
+    center: { lat: 40.13903, lng: -75.212611 }
+  });
+
+  marker = new google.maps.Marker({
+    map: map,
+    draggable: true,
+    animation: google.maps.Animation.DROP,
+    position: { lat: 40.13903, lng: -75.212611 }
+  });
+  marker.addListener("click", toggleBounce);
+}
+
+function toggleBounce() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,6 @@
 var db = require("../models");
 var passport = require("../config/passport");
+var Zillow = require('zillow-node');
 
 module.exports = function(app) {
   app.post("/api/login", passport.authenticate("local"), function(req, res) {
@@ -35,5 +36,32 @@ module.exports = function(app) {
         id: req.user.id
       });
     }
+  });
+
+  app.get("/api/zillow", function(req, res) {
+    var z = new Zillow("X1-ZWz17idc5wq96z_6ikyi");
+
+    var params = {
+      address: "120 N Bethlehem Pike Apt 302a",
+      citystatezip: "Fort Washington, Pa",
+      rentzestimate: "true"
+    };
+
+    z.get("GetSearchResults", params).then(function(results) {
+      var relInfo = {
+        streetAdd: results.response.results.result[0].address.street,
+        cityAdd: results.response.results.result[0].address.city,
+        stateAdd: results.response.results.result[0].address.state,
+        zipAdd: results.response.results.result[0].address.zipcode,
+        lat: results.response.results.result[0].address.latitude,
+        lng: results.response.results.result[0].address.longitude,
+        zestimate: results.response.results.result[0].zestimate.amount,
+        rentzestimate: results.response.results.result[0].rentzestimate.amount,
+        homeDetailsURL: results.response.results.result[0].links.homedetails
+      };
+      // console.log(relInfo);
+      res.json(relInfo);
+
+    });
   });
 };
