106189a67cca5b2bf3a68c1d48190faeaafc8ce2
sahasra101 <ajaykiri7@gmail.com>
2019-12-05 15:45:56 -0500

    integrated zillow api and googlemaps

--- public/js/userInput.js
+++ public/js/userInput.js
@@ -56,138 +56,150 @@ $(document).ready(function() {
     $.post("/api/address", addZillow, function(data) {
       console.log("zillow api data: ", data);
       // data should be the relevant info from the Zillow API call
+      $("#zillow-display").append(
+        "<p id='rent-est'>RestZestimate: " + data.rentzestimate + "</p>"
+      );
+      $("#zillow-display").append(
+        "<p id='est'>Zestimate: " + data.zestimate + "</p>"
+      );
+      $("#zillow-display").append(
+        "<p id='zillow-url'>Zillow Info: <a href='" +
+          data.homeDetailsURL +
+          "' target='_blank'>Zillow Details</a></p>"
+      );
     });
-
-    purchasePrice =
-      $("#purchasePrice")
-        .val()
-        .trim() || 0;
-    console.log(purchasePrice);
-
-    monthlyRent =
-      $("#monthlyRent")
-        .val()
-        .trim() || 0;
-    console.log("monthly rent: " + monthlyRent);
-
-    yearlyRent = monthlyRent * 12;
-    console.log("yearly rent: " + yearlyRent);
-
-    vacancyRate = document.getElementById("vacancyRate").value / 100 || 0;
-
-    console.log("vacancy rate: " + vacancyRate);
-
-    yearOneRent = yearlyRent - vacancyRate * yearlyRent;
-    console.log("yearOneRent: " + yearOneRent);
-
-    yearlyTaxes =
-      $("#yearlyTaxes")
-        .val()
-        .trim() || 0;
-    console.log("yearly taxes: " + yearlyTaxes);
-
-    yearlyInsurance =
-      $("#yearlyInsurance")
-        .val()
-        .trim() || 0;
-    console.log("yearly taxes: " + yearlyInsurance);
-
-    yearlyMaintenance =
-      $("#yearlyMaintenance")
-        .val()
-        .trim() || 0;
-    console.log("yearly maintenance cost: " + yearlyMaintenance);
-
-    monthlyUtilities =
-      $("#monthlyUtilities")
-        .val()
-        .trim() || 0;
-    console.log("monthly utilities: " + monthlyUtilities);
-
-    yearlyUtilities = monthlyUtilities * 12;
-    console.log("yearly Utilities: " + yearlyUtilities);
-
-    closingCosts =
-      $("#closingCosts")
-        .val()
-        .trim() || 0;
-    console.log("closing costs: " + closingCosts);
-
-    renovationCosts =
-      $("#renovationCosts")
-        .val()
-        .trim() || 0;
-    console.log("renovation Costs: " + renovationCosts);
-
-    netRent =
-      yearOneRent -
-      yearlyTaxes -
-      yearlyMaintenance -
-      yearlyUtilities -
-      yearlyInsurance;
-    console.log("Net Rent: " + netRent);
-
-    netRentToFixed = netRent.toFixed(2);
-    console.log("Net Rent to Fixed: " + netRentToFixed);
-
-    yearOneCost =
-      parseInt(purchasePrice) +
-      parseInt(closingCosts) +
-      parseInt(renovationCosts);
-    console.log("total year one costs: " + yearOneCost);
-
-    yearOneYield = (netRentToFixed / yearOneCost) * 100 || 0;
-    console.log("total yield = " + yearOneYield);
-
-    // Want to convert the number to only include two decimals.
-    yieldToFixed = yearOneYield.toFixed(2);
-
-    var resultsTable = $("<table class='table'>");
-    var resultsHead = $("<thead>");
-    var resultsTR = $("<tr>");
-    var resultsTH = $("<th scope='col'>Address</th>");
-    var resultsTH2 = $("<th scope='col'>Initial Investment</th>");
-    var resultsTH3 = $("<th scope='col'>Year One Return</th>");
-    var resultsTH4 = $("<th scope='col'>Yield</th>");
-
-    resultsTR.append(resultsTH, resultsTH2, resultsTH3, resultsTH4);
-    resultsHead.append(resultsTR);
-    resultsTable.append(resultsHead);
-
-    var tBody = $("<tbody>");
-    var tRow = $("<tr>");
-    var td = $("<td>" + address + "</td>");
-    var td2 = $("<td>" + "$" + yearOneCost + "</td>");
-    var td3 = $("<td>" + "$" + netRentToFixed + "</td>");
-    var td4 = $("<td>" + yieldToFixed + "%" + "</td>");
-
-    tRow.append(td, td2, td3, td4);
-    tBody.append(tRow);
-    resultsTable.append(tBody);
-
-    var buttonRow = $("<tr>");
-    var button = $(
-      "<button type='button' class='btn btn-primary saveSearch' data-id='" +
-        resultsArray.length +
-        "'> Save Search </button>"
-    );
-
-    resultsArray.push([address, yearOneCost, netRentToFixed, yieldToFixed]);
-
-    console.log(resultsArray);
-
-    buttonRow.append(button);
-    resultsTable.append(buttonRow);
-
-    $(".results").append(resultsTable);
   });
+
+  purchasePrice =
+    $("#purchasePrice")
+      .val()
+      .trim() || 0;
+  console.log(purchasePrice);
+
+  monthlyRent =
+    $("#monthlyRent")
+      .val()
+      .trim() || 0;
+  console.log("monthly rent: " + monthlyRent);
+
+  yearlyRent = monthlyRent * 12;
+  console.log("yearly rent: " + yearlyRent);
+
+  vacancyRate = document.getElementById("vacancyRate").value / 100 || 0;
+
+  console.log("vacancy rate: " + vacancyRate);
+
+  yearOneRent = yearlyRent - vacancyRate * yearlyRent;
+  console.log("yearOneRent: " + yearOneRent);
+
+  yearlyTaxes =
+    $("#yearlyTaxes")
+      .val()
+      .trim() || 0;
+  console.log("yearly taxes: " + yearlyTaxes);
+
+  yearlyInsurance =
+    $("#yearlyInsurance")
+      .val()
+      .trim() || 0;
+  console.log("yearly taxes: " + yearlyInsurance);
+
+  yearlyMaintenance =
+    $("#yearlyMaintenance")
+      .val()
+      .trim() || 0;
+  console.log("yearly maintenance cost: " + yearlyMaintenance);
+
+  monthlyUtilities =
+    $("#monthlyUtilities")
+      .val()
+      .trim() || 0;
+  console.log("monthly utilities: " + monthlyUtilities);
+
+  yearlyUtilities = monthlyUtilities * 12;
+  console.log("yearly Utilities: " + yearlyUtilities);
+
+  closingCosts =
+    $("#closingCosts")
+      .val()
+      .trim() || 0;
+  console.log("closing costs: " + closingCosts);
+
+  renovationCosts =
+    $("#renovationCosts")
+      .val()
+      .trim() || 0;
+  console.log("renovation Costs: " + renovationCosts);
+
+  netRent =
+    yearOneRent -
+    yearlyTaxes -
+    yearlyMaintenance -
+    yearlyUtilities -
+    yearlyInsurance;
+  console.log("Net Rent: " + netRent);
+
+  netRentToFixed = netRent.toFixed(2);
+  console.log("Net Rent to Fixed: " + netRentToFixed);
+
+  yearOneCost =
+    parseInt(purchasePrice) +
+    parseInt(closingCosts) +
+    parseInt(renovationCosts);
+  console.log("total year one costs: " + yearOneCost);
+
+  yearOneYield = (netRentToFixed / yearOneCost) * 100 || 0;
+  console.log("total yield = " + yearOneYield);
+
+  // Want to convert the number to only include two decimals.
+  yieldToFixed = yearOneYield.toFixed(2);
+
+  var resultsTable = $("<table class='table'>");
+  var resultsHead = $("<thead>");
+  var resultsTR = $("<tr>");
+  var resultsTH = $("<th scope='col'>Address</th>");
+  var resultsTH2 = $("<th scope='col'>Initial Investment</th>");
+  var resultsTH3 = $("<th scope='col'>Year One Return</th>");
+  var resultsTH4 = $("<th scope='col'>Yield</th>");
+
+  resultsTR.append(resultsTH, resultsTH2, resultsTH3, resultsTH4);
+  resultsHead.append(resultsTR);
+  resultsTable.append(resultsHead);
+
+  var tBody = $("<tbody>");
+  var tRow = $("<tr>");
+  var td = $("<td>" + address + "</td>");
+  var td2 = $("<td>" + "$" + yearOneCost + "</td>");
+  var td3 = $("<td>" + "$" + netRentToFixed + "</td>");
+  var td4 = $("<td>" + yieldToFixed + "%" + "</td>");
+
+  tRow.append(td, td2, td3, td4);
+  tBody.append(tRow);
+  resultsTable.append(tBody);
+
+  var buttonRow = $("<tr>");
+  var button = $(
+    "<button type='button' class='btn btn-primary saveSearch' data-id='" +
+      resultsArray.length +
+      "'> Save Search </button>"
+  );
+
+  resultsArray.push([address, yearOneCost, netRentToFixed, yieldToFixed]);
+
+  console.log(resultsArray);
+
+  buttonRow.append(button);
+  resultsTable.append(buttonRow);
+
+  $(".results").append(resultsTable);
 });
+// });
 
 // eslint-disable-next-line no-unused-vars
 function initMap() {
   var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 8,
-    center: { lat: -34.397, lng: 150.644 }
+    zoom: 13,
+    center: { lat: 40.139, lng: -75.212611 }
   });
   var geocoder = new google.maps.Geocoder();
 
